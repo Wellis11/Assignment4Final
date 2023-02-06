3742c235651d6410bdb8a9c59e63fa55e3051867
William Ellis <wellis@usf.edu>
2023-02-06 07:52:54 -0500

    Final Script

--- Assignment4Script.R
+++ Assignment4Script.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+
 bpdata<- read.csv("bpdata.csv", header=T)
 bpdata
 print(bpdata)
@@ -7,3 +8,18 @@ BpBoxplot<-ggplot(bpdata, aes(x=as.factor(finaldecision), y=bloodp)) +
   xlab("finaldecision")
 
 print(BpBoxplot)
+
+
+BpHisto<- bpdata %>%
+  ggplot( aes(x=bloodp)) +
+  geom_histogram( binwidth=20, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  ggtitle("Blood Pressure Histogram") 
+
+print(BpHisto)
+
+meanbyfinal<- aggregate.data.frame(x=bpdata$bloodp, 
+                                   by=list(bpdata$finaldecision),
+                                   FUN=mean)
+
+print(meanbyfinal)
+
